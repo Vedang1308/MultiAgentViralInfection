f9fcb46cfd84134f5fc340bf1d980e356a20428a
Vedang <vedangavaghade01@gmail.com>
2026-06-17 21:46:45 -0700

    Fix hydra config-name

--- Phase_1/enacttom_loader.py
+++ Phase_1/enacttom_loader.py
@@ -37,6 +37,7 @@ def main():
     # We specify our custom LLaVA provider and task directory
     sys.argv = [
         "enacttom_loader.py",
+        "--config-name", "benchmark",
         "+model=llava-1.5",
         "+llm_provider=llava_local",
         "+task_dir=Others/EnactTom/data/enacttom/tasks",
